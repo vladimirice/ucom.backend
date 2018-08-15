9d6b21d1c77838749f5ab6bf367afb7b020fc543
vladimir <mr.vladimir.ice@gmail.com>
2018-08-15 16:41:46 +0300

    * pm2 ecosystem config is added * gitignore file is edited

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name      : 'uos_backend',
+    script    : 'bin/www',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+};
