143962a931ec5aa649912acf0b07e4b8cc905e01
vladimir <mr.vladimir.ice@gmail.com>
2018-09-10 11:04:40 +0300

    migration to add unique constraints to Users table

---
+++ migrations/20180910075623-alter-users-add-unique-constraints.js
@@ -0,0 +1,54 @@
+const TABLE_NAME = 'Users';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.changeColumn(TABLE_NAME, 'account_name', {
+      type: Sequelize.STRING,
+      required: true,
+      allowNull: false,
+      unique: true
+    })
+      .then(() => {
+        return queryInterface.changeColumn(TABLE_NAME, 'nickname', {
+          type: Sequelize.STRING,
+          required: true,
+          allowNull: false,
+          unique: true
+        })
+      })
+      .then(() => {
+        return queryInterface.changeColumn(TABLE_NAME, 'email', {
+          type: Sequelize.STRING,
+          required: true,
+          allowNull: false,
+          unique: true
+        })
+      })
+      .then(() => {
+        return queryInterface.changeColumn(TABLE_NAME, 'phone_number', {
+          type: Sequelize.STRING,
+          required: true,
+          allowNull: false,
+          unique: true
+        })
+      })
+      .then(() => {
+        return queryInterface.changeColumn(TABLE_NAME, 'public_key', {
+          type: Sequelize.STRING,
+          required: true,
+          allowNull: false,
+          unique: true
+        })
+      });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
