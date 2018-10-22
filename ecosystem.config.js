2a5c80f744d945d8bf08cd1afdbbc5b1da57e32f
vladimir <mr.vladimir.ice@gmail.com>
2018-10-22 11:29:58 +0300

    in process

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,102 +1,100 @@
 module.exports = {
   apps : [
     {
-      name      : 'uos_backend',
-      script    : 'bin/www',
-      watch: true,
-      instance_var: 'INSTANCE_ID',
+      name:           'uos_backend',
+      instance_var:   'INSTANCE_ID',
+      script:         'bin/www',
+      watch:          false,
+      autorestart:    true,
       env: {
-        "PORT": 3000,
-        "NODE_ENV": "development"
-      },
-      autorestart: true,
-      env_production : {
-        NODE_ENV: 'production',
-        watch: false,
+        PORT:         3002,
+        NODE_ENV:     'development',
+        watch:        true,
       },
       env_test : {
-        NODE_ENV: 'test'
-      }
-    },
-    {
-      name: 'uos_backend_importance_worker',
-      script: 'bin/worker-update-importance.js',
-      watch: false,
-      cron_restart: "* * * * *",
-      env: {
-        "NODE_ENV": "development"
+        PORT:         3000,
+        NODE_ENV:     'test',
       },
-      env_production : {
-        NODE_ENV: 'production',
-      },
-    },
-    {
-      name: 'uos_backend_ipfs_consumer',
-      script: 'bin/ipfs-consumer.js',
-      watch: true,
-      env: {
-        "NODE_ENV": "development"
-      },
-      env_production : {
-        NODE_ENV: 'production',
-        watch: false,
-      },
-      env_test : {
-        NODE_ENV: 'test',
-      },
-    },
-    {
-      name: 'uos_backend_blockchain_consumer',
-      script: 'bin/blockchain-consumer.js',
-      watch: true,
-      env: {
-        "NODE_ENV": "development"
-      },
-      env_production : {
-        NODE_ENV: 'production',
-        watch: false,
-      },
-      env_test : {
-        NODE_ENV: 'test',
+      env_production: {
+        PORT:         3000,
+        NODE_ENV:     'production',
       },
     },
 
-    // *********** staging applications ***********
+    // *********** staging applications *********** //
     {
-      name      : 'uos_backend_staging',
-      script    : 'bin/www',
-      watch: false,
-      instance_var: 'INSTANCE_ID',
+      name:           'staging_uos_backend',
+      instance_var:   'INSTANCE_ID',
+      script:         'bin/www',
+      watch:          false,
+      autorestart:    true,
       env: {
-        "PORT": 3001,
-        "NODE_ENV": "staging"
-      },
-      env_production : {
-        NODE_ENV: 'staging',
+        PORT:         3001,
+        NODE_ENV:     'staging',
       },
     },
     {
-      name: 'uos_backend_staging_importance_worker',
-      script: 'bin/worker-update-importance.js',
-      watch: false,
-      cron_restart: "* * * * *",
+      name:           'staging_uos_backend_blockchain_consumer',
+      script:         'bin/blockchain-consumer.js',
+      watch:          false,
+      autorestart:    true,
       env: {
-        "NODE_ENV": "staging"
-      },
-      env_production : {
-        NODE_ENV: 'staging',
+        NODE_ENV:     'staging',
       },
     },
-    {
-      name: 'uos_backend_staging_blockchain_consumer',
-      script: 'bin/blockchain-consumer.js',
-      watch: true,
-      env: {
-        "NODE_ENV": "staging"
-      },
-      env_production : {
-        NODE_ENV: 'staging',
-      },
-    },
+    // {
+    //   name: 'uos_backend_importance_worker',
+    //   script: 'bin/worker-update-importance.js',
+    //   watch: false,
+    //   cron_restart: "* * * * *",
+    //   env: {
+    //     "NODE_ENV": "development"
+    //   },
+    //   env_production : {
+    //     NODE_ENV: 'production',
+    //   },
+    // },
+    // {
+    //   name: 'uos_backend_ipfs_consumer',
+    //   script: 'bin/ipfs-consumer.js',
+    //   watch: true,
+    //   env: {
+    //     "NODE_ENV": "development"
+    //   },
+    //   env_production : {
+    //     NODE_ENV: 'production',
+    //     watch: false,
+    //   },
+    //   env_test : {
+    //     NODE_ENV: 'test',
+    //   },
+    // },
+    // {
+    //   name: 'uos_backend_blockchain_consumer',
+    //   script: 'bin/blockchain-consumer.js',
+    //   watch: true,
+    //   env: {
+    //     "NODE_ENV": "development"
+    //   },
+    //   env_production : {
+    //     NODE_ENV: 'production',
+    //     watch: false,
+    //   },
+    //   env_test : {
+    //     NODE_ENV: 'test',
+    //   },
+    // },
+    // {
+    //   name: 'uos_backend_staging_importance_worker',
+    //   script: 'bin/worker-update-importance.js',
+    //   watch: false,
+    //   cron_restart: "* * * * *",
+    //   env: {
+    //     "NODE_ENV": "staging"
+    //   },
+    //   env_production : {
+    //     NODE_ENV: 'staging',
+    //   },
+    // },
   ],
 };
