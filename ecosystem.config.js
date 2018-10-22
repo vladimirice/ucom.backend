af7453d0472a03275163fdfc86e0e5448d9c9c4b
vladimir <mr.vladimir.ice@gmail.com>
2018-10-22 15:43:33 +0300

    ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   apps : [
+    // *********** production applications *********** //
     {
       name:           'uos_backend',
       instance_var:   'INSTANCE_ID',
@@ -18,6 +19,34 @@ module.exports = {
         autorestart:  true,
       },
     },
+    {
+      name:           'production_uos_backend_blockchain_consumer',
+      script:         'bin/blockchain-consumer.js',
+      watch:          false,
+      autorestart:    false,
+      env: {
+        NODE_ENV:     'test',
+        watch:          false,
+        autorestart:    true,
+      },
+      env_production: {
+        NODE_ENV:     'production',
+        watch:          false,
+        autorestart:    false,
+      },
+    },
+    {
+      name: 'production_uos_backend_importance_worker',
+      script: 'bin/worker-update-importance.js',
+      watch: false,
+      cron_restart: '* * * * *',
+      env: {
+        NODE_ENV: 'test'
+      },
+      env_production : {
+        NODE_ENV: 'production',
+      },
+    },
 
     // *********** staging applications *********** //
     {
@@ -30,28 +59,35 @@ module.exports = {
         PORT:         3001,
         NODE_ENV:     'staging',
       },
+      env_production: {
+        PORT:         3001,
+        NODE_ENV:     'staging',
+      },
     },
     {
       name:           'staging_uos_backend_blockchain_consumer',
       script:         'bin/blockchain-consumer.js',
       watch:          false,
-      autorestart:    true,
+      autorestart:    false,
       env: {
         NODE_ENV:     'staging',
       },
+      env_production: {
+        NODE_ENV:     'staging',
+      },
     },
-    // {
-    //   name: 'uos_backend_importance_worker',
-    //   script: 'bin/worker-update-importance.js',
-    //   watch: false,
-    //   cron_restart: "* * * * *",
-    //   env: {
-    //     "NODE_ENV": "development"
-    //   },
-    //   env_production : {
-    //     NODE_ENV: 'production',
-    //   },
-    // },
+    {
+      name: 'staging_uos_backend_importance_worker',
+      script: 'bin/worker-update-importance.js',
+      watch: false,
+      cron_restart: '* * * * *',
+      env: {
+        NODE_ENV: 'staging'
+      },
+      env_production : {
+        NODE_ENV: 'staging',
+      },
+    },
     // {
     //   name: 'uos_backend_ipfs_consumer',
     //   script: 'bin/ipfs-consumer.js',
@@ -65,33 +101,6 @@ module.exports = {
     //   },
     //   env_test : {
     //     NODE_ENV: 'test',
-    //   },
-    // },
-    // {
-    //   name: 'uos_backend_blockchain_consumer',
-    //   script: 'bin/blockchain-consumer.js',
-    //   watch: true,
-    //   env: {
-    //     "NODE_ENV": "development"
-    //   },
-    //   env_production : {
-    //     NODE_ENV: 'production',
-    //     watch: false,
-    //   },
-    //   env_test : {
-    //     NODE_ENV: 'test',
-    //   },
-    // },
-    // {
-    //   name: 'uos_backend_staging_importance_worker',
-    //   script: 'bin/worker-update-importance.js',
-    //   watch: false,
-    //   cron_restart: "* * * * *",
-    //   env: {
-    //     "NODE_ENV": "staging"
-    //   },
-    //   env_production : {
-    //     NODE_ENV: 'staging',
     //   },
     // },
   ],
