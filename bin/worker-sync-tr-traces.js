621c69be2f58651014a726a6e2f058d3e060bad3
vlad <mr.vladimir.ice@gmail.com>
2018-12-02 22:19:50 +0300

    sync part is done

---
+++ bin/worker-sync-tr-traces.js
@@ -0,0 +1,10 @@
+const BlockchainTrTracesService     = require('../lib/eos/service/blockchain-tr-traces-service');
+const BlockchainTrTracesRepository  = require('../lib/eos/repository/blockchain-tr-traces-repository');
+
+(async () => {
+  await BlockchainTrTracesRepository.setSeqCurrentValByMaxNum();
+
+  await BlockchainTrTracesService.syncMongoDbAndPostgres();
+
+  console.log('worker had finished its work')
+})();
