8811017147bbb6fce6a396c3e4e4a481969c2b89
vlad <mr.vladimir.ice@gmail.com>
2018-12-07 16:42:42 +0300

    * remove wrong error message related to stake-with-unstake

--- pm2_production_logs.sh
+++ pm2_production_logs.sh
@@ -2,5 +2,4 @@
 . ~/.nvm/nvm.sh
 . ~/.bashrc
 
-tail -f /var/www/uos.app.backend/logs/app_error.log --lines=100
-#less /var/www/uos.app.backend/logs/app1.log
+tail -f /var/www/ucom.backend/logs/app_error.log --lines=100
