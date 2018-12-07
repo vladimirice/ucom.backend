055ef70f5eb2729e087f0aee1eb98a2dbef41ab1
vlad <mr.vladimir.ice@gmail.com>
2018-12-07 16:33:05 +0300

    * adjust backend deploy script * add sample line to bash_alias in order to test new production deploy folder

--- uos_backend_deploy_production.sh
+++ uos_backend_deploy_production.sh
@@ -2,6 +2,7 @@
 . ~/.nvm/nvm.sh
 . ~/.bashrc
 
+echo "!!!!!!!! Let's deploy on production !!!!!!!!"
 cd /var/www/ucom.backend
 pwd
 echo "Making git pull..."
@@ -10,7 +11,7 @@ echo "Lets make npm install"
 npm install --only=prod
 echo "Applying migrations..."
 NODE_ENV=production node_modules/.bin/sequelize db:migrate
-echo "Lets restart pm2"
+echo "Lets restart pm2 with update env and saving new configuration"
 /home/dev/.nvm/versions/node/v10.9.0/bin/pm2 restart ecosystem-production.config.js --update-env
 /home/dev/.nvm/versions/node/v10.9.0/bin/pm2 save
-echo "Deploy is finished"
+echo "!!!!!!!! Deploy on production is finished !!!!!!!!"
