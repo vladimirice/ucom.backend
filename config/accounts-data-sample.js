3185dc07ac9071010b007f3710470cea55c1d6b3
Vladimir Ice <mr.vladimir.ice@gmail.com>
2018-12-17 11:05:42 +0300

    Support. Gitignore to preserve upload/organization and sample config files

---
+++ config/accounts-data-sample.js
@@ -0,0 +1,46 @@
+module.exports = {
+  account_creator: {
+    account_name: 'accregistrar',
+    activePk: 'samlpe',
+    activePubKey: 'samlpe'
+  },
+  vlad: {
+    account_name: 'vladvladvlad',
+    brainkey: 'samlpe',
+    ownerPk: 'samlpe',
+    ownerPubKey: 'samlpe',
+    activePk: 'samlpe',
+    activePubKey: 'samlpe'
+  },
+  jane: {
+    account_name: 'janejanejane',
+    brainkey: 'samlpe',
+    ownerPk: 'samlpe',
+    ownerPubKey: 'samlpe',
+    activePk: 'samlpe',
+    activePubKey: 'samlpe',
+  },
+  petr: {
+    account_name: 'petrpetrpetr',
+    brainkey: 'samlpe',
+    ownerPk: 'samlpe',
+    ownerPubKey: 'samlpe',
+    activePk: 'samlpe',
+    activePubKey: 'samlpe',
+  },
+  rokky: {
+    account_name: 'rokkyrokkyro',
+    brainkey: 'samlpe',
+    ownerPk: 'samlpe',
+    ownerPubKey: 'samlpe',
+    activePk: 'samlpe',
+    activePubKey: 'samlpe',
+  },
+  block_producers: [
+    'calc1',
+    'calc2',
+    'calc3',
+    'calc4',
+    'calc5'
+  ],
+};
