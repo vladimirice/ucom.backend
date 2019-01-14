e8f3ac875912112b8e49cbb2d562548965b28a86
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-01-14 18:24:03 +0300

    in process

---
+++ bin/maintenance/entity-recalculate-current-vote.ts
@@ -0,0 +1,102 @@
+export {};
+
+type votedEntity = {
+  entity_id: number;
+  entity_name: string;
+  upvote: number;
+  downvote: number;
+  current_vote: number;
+};
+
+type votedEntitySet = {
+  [id: number]: votedEntity;
+};
+
+const knex = require('../../config/knex');
+
+(async () => {
+  const entityName = process.env.ENTITY === '1' ? 'posts     ' : 'comments  ';
+  const tableName = process.env.ENTITY === '1' ? 'posts' : 'comments';
+
+  const sql = `
+  SELECT COUNT(1), activity_type_id, entity_id_to, entity_name FROM users_activity
+    WHERE activity_type_id IN (2, 4) AND activity_group_id = 2
+     AND blockchain_status = 1
+    AND entity_name = '${entityName}'
+  GROUP BY entity_id_to, entity_name, activity_type_id
+  ORDER BY entity_id_to, entity_name DESC;
+  `;
+
+  const data = await knex.raw(sql);
+  const entities: votedEntitySet = {};
+
+  for (const row of data.rows) {
+    processEntity(entities, row);
+  }
+
+  await updateCurrentVote(entities, tableName);
+})();
+
+function processEntity(entities: votedEntitySet, row: any) {
+  if (!entities[row.entity_id_to]) {
+    entities[row.entity_id_to] = {
+      entity_id: row.entity_id_to,
+      entity_name: row.entity_name,
+      upvote: 0,
+      downvote: 0,
+      current_vote: 0,
+    };
+  }
+
+  if (row.activity_type_id === 2) {
+    entities[row.entity_id_to].upvote = +row.count;
+  } else {
+    entities[row.entity_id_to].downvote = +row.count;
+  }
+}
+
+async function updateCurrentVote(entities: votedEntitySet, tableName: string) {
+  let promises: any = [];
+
+  const batchSize: number = 200;
+  for (const entityId in entities) {
+    const entity = entities[entityId];
+
+    entity.current_vote = entity.upvote - entity.downvote;
+    const sql =
+      `UPDATE ${tableName} SET current_vote = ${+entity.current_vote} WHERE id = ${+entityId}`;
+
+    promises.push(knex.raw(sql));
+
+    if (promises.length === batchSize) {
+      console.log(`Lets process promises. Amount of updates are: ${promises.length}`);
+      await Promise.all(promises);
+      console.log('processed');
+      promises = [];
+    }
+  }
+
+  if (promises.length > 0) {
+    console.log(`Lets process remained promises. Amount of updates are: ${promises.length}`);
+    await Promise.all(promises);
+    console.log('processed');
+  }
+}
+
+async function setZeroCurrentVoteForInvalidOnlyTransactions() {
+  /*
+UPDATE posts set posts.current_vote = 0 WHERE posts.id IN (
+  SELECT posts.id
+  FROM posts
+         LEFT JOIN users_activity
+                   ON posts.id = users_activity.entity_id_to
+                     AND users_activity.entity_name = 'posts     '
+                     AND users_activity.blockchain_status = 1
+                     AND activity_type_id IN (2, 4)
+                     AND activity_group_id = 2
+  WHERE
+    users_activity.id IS NULL
+    AND posts.current_vote > 0
+);
+*/
+}
