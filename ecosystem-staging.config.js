f1d5086eecf23fe584e429120383a7df0ba19664
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-02-06 10:28:03 +0300

    Sync transaction traces worker staging run interval has been changed - now it is the same as for production

--- ecosystem-staging.config.js
+++ ecosystem-staging.config.js
@@ -115,7 +115,7 @@ module.exports = {
       name: `${NODE_ENV}_worker_sync_tr_traces`,
       script: 'bin/worker-sync-tr-traces.js',
       watch: false,
-      cron_restart: '*/2 * * * *',
+      cron_restart: '0 * * * *',
       env: {
         NODE_ENV: NODE_ENV
       },
