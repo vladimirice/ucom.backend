e0e69f7ad8f7968319368e236579e480ad204e21
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-02-15 16:53:53 +0300

    Add start-finish console log to stats worker

--- bin/worker-stats-calculate-event-params.ts
+++ bin/worker-stats-calculate-event-params.ts
@@ -1,7 +1,13 @@
+/* eslint-disable no-console */
 import EntityCalculationService = require('../lib/stats/service/entity-calculation-service');
 
-export {};
-
 (async () => {
+  console.log('Lets run the worker');
+  const startTime = process.hrtime();
+
   await EntityCalculationService.updateEntitiesDeltas();
+  const endTime = process.hrtime(startTime);
+  console.log(`Worker has finished its work. Execution time: ${endTime[1] / 1000000} ms`);
 })();
+
+export {};
