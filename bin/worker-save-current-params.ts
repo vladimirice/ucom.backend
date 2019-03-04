ca8e669bfb94417a2e016249d08952eb213a1c74
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-03-04 15:46:48 +0300

    New command has been added to worker-save-current-params, #35

--- bin/worker-save-current-params.ts
+++ bin/worker-save-current-params.ts
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import { EntityJobExecutorService } from '../lib/stats/service/entity-job-executor-service';
+import EntityTotalsCalculator = require('../lib/stats/service/entity-totals-calculator');
 
 (async () => {
   const startTime = process.hrtime();
@@ -8,7 +9,16 @@ import { EntityJobExecutorService } from '../lib/stats/service/entity-job-execut
   await EntityJobExecutorService.processEntityEventParam();
 
   const endTime = process.hrtime(startTime);
-  console.log(`Params are saved. Time: ${endTime[1] / 1000000} ms`);
+  console.log(`====== Params have been saved. Time: ${endTime[1] / 1000000} ms`);
+
+  // ==========================================
+  const totalsStartTime = process.hrtime();
+
+  console.log("Then let's save total params");
+  await EntityTotalsCalculator.calculate();
+
+  const totalsEndTime = process.hrtime(totalsStartTime);
+  console.log(`==== Total params have been saved. Time: ${totalsEndTime[1] / 1000000} ms`);
 })();
 
 export {};
