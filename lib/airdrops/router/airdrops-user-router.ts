f0968090b0e283cbef86e68a7b91738e579a5c4b
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-03-12 17:06:34 +0300

    Support, resolves #46

--- lib/airdrops/router/airdrops-user-router.ts
+++ lib/airdrops/router/airdrops-user-router.ts
@@ -1,5 +1,6 @@
 import GithubAuthService = require('../../github/service/github-auth-service');
 import AuthService = require('../../auth/authService');
+import { BadRequestError } from '../../api/errors';
 
 const express = require('express');
 
@@ -9,6 +10,10 @@ require('express-async-errors');
 
 AirdropsUserRouter.get('/:airdrop_id/user', async (req, res) => {
   const token = req.cookies[GithubAuthService.getCookieName()];
+
+  if (!token) {
+    throw new BadRequestError('Github token should be provided via cookie', 401);
+  }
 
   AuthService.extractUsersExternalIdByTokenOrError(token);
 
