a0caf6ae0df0251fc398292963855ab9b1753ac0
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-03-14 18:08:38 +0300

    jest template has been improved, resolves #65

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
+const DEFAULT_TEST_REGEX = '.test.ts$';
+
 const { getConfig } = require('./test/template-config');
 
-let filePattern = process.env.test_pattern || null;
-if (filePattern) {
-  filePattern += '.*';
+let prefix = process.env.test_pattern || '';
+if (prefix !== '') {
+  prefix = process.env.to_exclude ? `^((?!${prefix}).)*` : `${prefix}.*`;
 }
 
 const testsFolder = process.env.test_dir || null;
+const pattern = `${prefix}${DEFAULT_TEST_REGEX}`;
 
-module.exports = getConfig(filePattern, testsFolder);
+module.exports = getConfig(pattern, testsFolder);
