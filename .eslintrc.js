7f041dc5f47412ac510ce86eb21c17cb9bb0914d
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-04-01 11:10:27 +0300

    support. new eslint plugin - unicorn

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:sonarjs/recommended",
+    "plugin:unicorn/recommended"
   ],
   "env": {
     "node": true,
