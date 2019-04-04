80d48a97d89b221e48d2538d5c69ea298bc09b41
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-04-04 15:22:27 +0300

    fix missed import, #90

--- bin/app-uploader.js
+++ bin/app-uploader.js
@@ -2,5 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const http = require('http');
 const { app } = require('../lib/uploader/uploader-application');
+const ApiErrorAndLoggingHelper = require('../lib/api/helpers/api-error-and-logging-helper');
 const server = http.createServer(app);
 ApiErrorAndLoggingHelper.initServerOrException(app, server);
--- bin/app-uploader.ts
+++ bin/app-uploader.ts
@@ -1,6 +1,8 @@
 const http = require('http');
 const { app } = require('../lib/uploader/uploader-application');
 
+const ApiErrorAndLoggingHelper = require('../lib/api/helpers/api-error-and-logging-helper');
+
 const server = http.createServer(app);
 ApiErrorAndLoggingHelper.initServerOrException(app, server);
 
