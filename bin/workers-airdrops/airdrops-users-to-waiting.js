9554b57bed02b9a4fa599ef7b31f079ee44ad0d6
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-04-10 15:24:20 +0300

    worker is added, script-sender is added, resloves #61

--- bin/workers-airdrops/airdrops-users-to-waiting.js
+++ bin/workers-airdrops/airdrops-users-to-waiting.js
@@ -4,12 +4,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const AirdropsUsersToWaitingService = require("../../lib/airdrops/service/status-changer/airdrops-users-to-waiting-service");
 const DatetimeHelper = require("../../lib/common/helper/datetime-helper");
 const EosApi = require('../../lib/eos/eosApi');
-const AIRDROP_ID = 1;
 (async () => {
     EosApi.initWalletApi();
     console.log(`${DatetimeHelper.currentDatetime()}. Lets run the worker`);
     const startTime = process.hrtime();
-    await AirdropsUsersToWaitingService.process(AIRDROP_ID);
+    await AirdropsUsersToWaitingService.process(100);
     const endTime = process.hrtime(startTime);
     console.log(`Worker has finished its work. Execution time is: ${endTime[1] / 1000000} ms`);
 })();
