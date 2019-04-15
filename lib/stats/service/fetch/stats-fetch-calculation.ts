4469a969b9591d4efa2805b7cadc344bc2a8637d
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-04-15 11:08:29 +0300

    optimisation notice

--- lib/stats/service/fetch/stats-fetch-calculation.ts
+++ lib/stats/service/fetch/stats-fetch-calculation.ts
@@ -10,6 +10,8 @@ class StatsFetchCalculation {
     const newData = moment().subtract(params.windowIntervalHours, 'hours');
     const createdAtAsString = newData.utc().format('YYYY-MM-DD HH:mm:ss');
 
+    // #optimization - LATERAL JOIN
+    // see https://medium.com/kkempin/postgresqls-lateral-join-bfd6bd0199df
     return Promise.all([
       EntityEventRepository.findLastRowsGroupedByEntity(
         `event_type = ${params.initialEventType} AND entity_name = '${params.entityName}'`,
