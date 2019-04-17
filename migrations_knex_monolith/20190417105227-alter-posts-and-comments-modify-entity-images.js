476340e8f71518b7cbc97c11441e790f8eaf95b5
Vladimir Ice <mr.vladimir.ice@gmail.com>
2019-04-17 16:19:56 +0300

    change migration, #105

--- migrations_knex_monolith/20190417105227-alter-posts-and-comments-modify-entity-images.js
+++ migrations_knex_monolith/20190417105227-alter-posts-and-comments-modify-entity-images.js
@@ -13,25 +13,6 @@ exports.up = (knex) => {
           OR  entity_images = '""';
 
     ALTER TABLE ${TABLE_NAME} ALTER COLUMN entity_images SET NOT NULL;
-
-    ALTER TABLE ${TABLE_NAME} ADD CONSTRAINT ${TABLE_NAME}_entity_images_max_length 
-      CHECK (
-          pg_column_size(entity_images) > 0
-          AND pg_column_size(entity_images) < 10000
-          AND entity_images != '"{}"'
-          AND entity_images != '""'
-          AND entity_images != '"null"'
-      );
-
-    ALTER TABLE comments ALTER COLUMN entity_images DROP DEFAULT;
-    ALTER TABLE comments ADD CONSTRAINT comments_entity_images_max_length 
-      CHECK (
-          pg_column_size(entity_images) > 0
-          AND pg_column_size(entity_images) < 10000
-          AND entity_images != '"{}"'
-          AND entity_images != '""'
-          AND entity_images != '"null"'
-      );
 `;
 
   return knex.raw(sql);
