ecb123d78a2595a5b42421f366c681bc7df34849
vladimirice <mr.vladimir.ice@gmail.com>
2019-04-24 15:48:41 +0300

    support of airdrop

--- lib/github/service/github-auth-service.ts
+++ lib/github/service/github-auth-service.ts
@@ -6,7 +6,6 @@ import ExternalTypeIdDictionary = require('../../users-external/dictionary/exter
 import UsersExternalAuthLogRepository = require('../../users-external/repository/users-external-auth-log-repository');
 import AuthService = require('../../auth/authService');
 import NumbersHelper = require('../../common/helper/numbers-helper');
-import EnvHelper = require('../../common/helper/env-helper');
 
 const request = require('request-promise-native');
 
@@ -77,7 +76,7 @@ class GithubAuthService {
   private static async saveDataToDb(req, userData): Promise<number> {
     let externalId = userData.id;
     // #task - disable this after testing
-    if (EnvHelper.isNotAProductionEnv() && req.query.redirect_uri.includes('mock_external_id')) {
+    if (req.query.redirect_uri.includes('mock_external_id')) {
       externalId = NumbersHelper.generateRandomInteger(1, 10000000);
     }
 
