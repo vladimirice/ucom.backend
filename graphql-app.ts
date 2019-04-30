1b9735c9a3c194a15416f4e940dfba013e010dce
vladimirice <mr.vladimir.ice@gmail.com>
2019-04-30 12:42:39 +0300

    test

--- graphql-app.ts
+++ graphql-app.ts
@@ -668,25 +668,25 @@ const server = new ApolloServer({
   },
 });
 
-// app.use((req, res, next) => {
-//   const allowedOrigins = config.cors.allowed_origins;
-//
-//   const { origin } = req.headers;
-//   if (allowedOrigins.includes(origin)) {
-//     res.setHeader('Access-Control-Allow-Origin', 'https://staging.u.community');
-//   }
-//
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     `X-Requested-With,content-type,Authorization,${CommonHeaders.TOKEN_USERS_EXTERNAL_GITHUB},Cookie`,
-//   );
-//
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//
-//   next();
-// });
+app.use((req, res, next) => {
+  const allowedOrigins = config.cors.allowed_origins;
+
+  const { origin } = req.headers;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://staging.u.community');
+  }
+
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    `X-Requested-With,content-type,Authorization,${CommonHeaders.TOKEN_USERS_EXTERNAL_GITHUB},Cookie`,
+  );
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  next();
+});
 
 // @ts-ignore
 function determineOrigin() {
