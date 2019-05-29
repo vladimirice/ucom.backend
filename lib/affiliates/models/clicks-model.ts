7e7a546247b8c4a92287e343b160921e2be51ebd
vladimirice <mr.vladimir.ice@gmail.com>
2019-05-29 10:25:58 +0300

    in process

---
+++ lib/affiliates/models/clicks-model.ts
@@ -0,0 +1,54 @@
+import AffiliatesModelProvider = require('../service/affiliates-model-provider');
+import { IModelDto } from '../../common/interfaces/common-model-interfaces';
+import RepositoryHelper = require('../../common/repository/repository-helper');
+const { Model } = require('objection');
+
+const offersTableName = AffiliatesModelProvider.getOffersTableName();
+
+class ClicksModel extends Model implements IModelDto {
+  readonly id!:           number;
+
+  readonly user_id!:      number;
+  readonly account_name!: string;
+  readonly offer_id!:     number;
+
+  readonly created_at!:   Date;
+  readonly updated_at!:   Date;
+
+  public static getTableName(): string {
+    return AffiliatesModelProvider.getStreamsTableName();
+  }
+
+  $afterGet() {
+    RepositoryHelper.convertStringFieldsToNumbers(
+      this,
+      this.getNumericalFields(),
+      this.getNumericalFields(),
+    );
+  }
+
+  static get relationMappings() {
+    const OffersModel = require('./offers-model.js');
+
+    return {
+      offer: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: OffersModel,
+        join: {
+          from: `${this.getTableName()}.offer_id`,
+          to: `${offersTableName}.id`,
+        }
+      }
+    }
+  }
+
+  getNumericalFields(): string[] {
+    return [
+      'id',
+      'user_id',
+      'offer_id',
+    ];
+  }
+}
+
+export = ClicksModel;
