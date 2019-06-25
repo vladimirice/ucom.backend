0acadb2938495f0922346d5fae3ea5c798306494
vladimirice <mr.vladimir.ice@gmail.com>
2019-06-25 20:15:17 +0300

    support

--- test/helpers/posts/posts-graphql-request.ts
+++ test/helpers/posts/posts-graphql-request.ts
@@ -79,6 +79,30 @@ class PostsGraphqlRequest {
     return GraphqlRequestHelper.makeRequestFromQueryPartsByFetch([part], key);
   }
 
+  public static async getMainPageTopPublicationsForAll() {
+    const params = {
+      filters: {
+        post_type_ids: [PostTypes.MEDIA],
+        entity_names_from: [
+          EntityNames.USERS,
+          EntityNames.ORGANIZATIONS,
+        ],
+        entity_names_for: [
+          EntityNames.USERS,
+          EntityNames.ORGANIZATIONS,
+        ],
+      },
+      order_by: '-current_rate',
+      page: 1,
+      per_page: 10,
+    };
+
+    const part = GraphQLSchema.getPostsFeedQueryPart(params);
+    const key = 'posts_feed';
+
+    return GraphqlRequestHelper.makeRequestFromQueryPartsByFetch([part], key);
+  }
+
   public static async getUsersMainPageFeed() {
     const params = {
       filters: {
